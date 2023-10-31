f102084090b8ed873fcf31cf0c96f1b03cb89626
Shahad Youhanna <syou320@aucklanduni.ac.nz>
2023-11-01 12:28:23 +1300

    added client.js functions

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,100 @@
-// empty for now
+document.addEventListener("DOMContentLoaded", () => {
+    // This ensures that our JavaScript doesn't run until the HTML is fully loaded.
+
+    // Grabbing references to important DOM elements
+    const loginForm = document.getElementById('loginForm');
+    const logoutButton = document.getElementById('logoutButton');
+    const newAccountForm = document.getElementById('newAccountForm');
+
+    // Adding event listeners
+    if (loginForm) {
+        loginForm.addEventListener('submit', handleLogin);
+    }
+
+    if (logoutButton) {
+        logoutButton.addEventListener('click', handleLogout);
+    }
+
+    if (newAccountForm) {
+        newAccountForm.addEventListener('submit', handleNewAccount);
+    }
+});
+
+function handleLogin(event) {
+    event.preventDefault();
+    // Extract data from form
+    const formData = new FormData(event.target);
+    const username = formData.get('username');
+    const password = formData.get('password');
+
+    // Make fetch request to server
+    fetch("/login", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the response from the server
+        if (data.success) {
+            window.location.href = "/";
+        } else {
+            alert(data.message);
+        }
+    });
+}
+
+function handleLogout() {
+    fetch("/logout", {
+        method: 'GET',
+        credentials: 'include',
+    })
+    .then(response => {
+        window.location.href = "/login";
+    });
+}
+
+function handleNewAccount(event) {
+    event.preventDefault();
+
+    // input fields with these IDs from our form
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const name = document.getElementById("name").value;
+
+    // Check client-side if passwords match
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    // Send a POST request to create a new account
+    fetch("/newAccount", {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password, name })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            // Redirect or inform the user of successful account creation
+            window.location.href = "/login";
+        } else {
+            // Handle any errors or inform the user
+            alert(data.message || "An error occurred. Please try again.");
+        }
+    })
+    .catch(error => {
+        console.error("Error creating new account:", error);
+        alert("An error occurred. Please try again.");
+    });
+}
+
+
