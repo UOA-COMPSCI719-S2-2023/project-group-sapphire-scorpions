7fd188aa535751b2d3d30e7eb8a6a476af2da464
Shahad Youhanna <syou320@aucklanduni.ac.nz>
2023-11-01 09:36:55 +1300

    slight changes

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -32,13 +32,3 @@ create table messages (
     foreign key (senderId) references users(id),
     foreign key (receiverId) references users(id)
 );
-
-insert into users (id, username, password, name) values
-    (1, 'user1', 'pa55word', 'Alice'),
-    (2, 'user2', 'pa55word', 'Bob');
-
-insert into messages (id, senderId, receiverId, timestamp, content) values
-    (1, 1, 2, datetime('2021-05-15 15:00:00'), 'Hi, Bob!'),
-    (2, 2, 1, datetime('2021-05-15 15:02:00'), 'Hi, Alice!'),
-    (3, 1, 2, datetime('2021-05-15 15:04:00'), 'I like pie.'),
-    (4, 2, 1, datetime('2021-05-15 15:10:00'), 'COMPSCI 719 is awesome! And I like pie also too.');
